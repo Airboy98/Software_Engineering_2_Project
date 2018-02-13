04cf9e40cc69ae3645f4a6994f5265065562a2e4
Mayur Bhakta <32318210+mBhakta95@users.noreply.github.com>
2018-02-12 22:11:33 -0600

    Folder For GUI
    Got GUI working with 2 different scenes

---
+++ trendAssistGUI/src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class Controller{
+
+
+    public void logInButtonAction(ActionEvent event) throws IOException{
+
+        Parent homePageParent = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+        Scene homePageScene = new Scene(homePageParent);
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appStage.setScene(homePageScene);
+        appStage.show();
+    }
+}
