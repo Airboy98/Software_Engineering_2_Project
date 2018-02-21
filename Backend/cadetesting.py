547543d694209e926e8a9d553aa415096cd73e36
MichaelCadeWall <32107907+MichaelCadeWall@users.noreply.github.com>
2018-02-21 12:41:14 -0600

    sql works?

--- Backend/cadetesting.py
+++ Backend/cadetesting.py
@@ -2,6 +2,7 @@ import csv
 import argparse
 import os
 import sys
+import pymysql
 def whatdaymonth(lst):
     day=int(lst[0][3]+lst[0][4])
     if(day<7):
@@ -18,26 +19,36 @@ def whatdayyear(lst):
     day=whatdaymonth(lst)
     month=int(lst[0][0]+lst[0][1])
     return day*month
-try:
-    parser = argparse.ArgumentParser(description='upload excel csv file')
-    parser.add_argument('upload',help='put the path address as the arugemnt Ex. cadetesting.py C:\\Users\excel.csv')
-except:
-    print("Didnt work")
-    exit()
+# try:
+#     parser = argparse.ArgumentParser(description='upload excel csv file')
+#     parser.add_argument('upload',help='put the path address as the arugemnt Ex. cadetesting.py C:\\Users\excel.csv')
+# except:
+#     print("Didnt work")
+#     exit()
+#
+# args = parser.parse_args()
+#
+#
+# with open(sys.argv[1], 'r') as csvfile:
+#     timeReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     lst = []
+#     for row in timeReader:
+#        lst.append(''.join(row).split(","))
+#
+# del lst[0]
+# print(lst[0])
+# size = len(lst)
+#
+# for x in range(0,size):
+#     print(lst[x][0],whatdaymonth(lst[x]),whatdayyear(lst[x]),lst[x][2][0:3],lst[x][1])
 
-args = parser.parse_args()
 
+conn = pymysql.connect(host="localhost",user="root",passwd="0000",db="salesdata",autocommit=True)
+print('made it here')
+x = conn.cursor()
+print("made it here")
+x.execute("""INSERT INTO apr VALUES('1mon',256)""")
 
-with open(sys.argv[1], 'r') as csvfile:
-    timeReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    lst = []
-    for row in timeReader:
-       lst.append(''.join(row).split(","))
+print("didnt work")
 
-del lst[0]
-print(lst[0])
-size = len(lst)
-
-for x in range(0,size):
-    print(lst[x][0],whatdaymonth(lst[x]),whatdayyear(lst[x]),lst[x][2][0:3],lst[x][1])
-
+exit()
