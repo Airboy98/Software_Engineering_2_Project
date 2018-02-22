b6a012dc25452fc6503465772843f4cc69dc0c8f
Cade Wall <cade.wall@gmail.com>
2018-02-22 16:30:19 -0600

    autocommit testing

--- Backend/DBconnection.py
+++ Backend/DBconnection.py
@@ -4,7 +4,7 @@ import pymysql
 from passlib.hash import pbkdf2_sha256
 
 
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='0000', db='accounts')
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='0000', db='accounts',autocommit=True)
 
 cur = conn.cursor()
 
@@ -24,7 +24,7 @@ if select=="1":
     phash = pbkdf2_sha256.hash(pass_nh)
     #print("INSERT INTO `users` (`username`,`passhash`,`position`) VALUES ('" + uname + "','" + phash + "','" + ID + "');")
     cur.execute("INSERT INTO `users` (`username`,`passhash`,`position`) VALUES ('" + uname + "','" + phash + "','" + ID + "');")
-    conn.commit()
+    #conn.commit()
 
 
 elif select=="2":
