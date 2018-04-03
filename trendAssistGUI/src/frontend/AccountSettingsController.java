f4059b64bbd80de5a07c3ebcd2e5cb6d769ce98a
Mayur Bhakta <32318210+mBhakta95@users.noreply.github.com>
2018-04-02 22:06:14 -0500

    more changes
    xD

--- trendAssistGUI/src/frontend/AccountSettingsController.java
+++ trendAssistGUI/src/frontend/AccountSettingsController.java
@@ -3,13 +3,20 @@ package frontend;
 import Encryption.PasswordENC;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -124,10 +131,33 @@ public class AccountSettingsController implements Initializable {
             e.printStackTrace();
         }
         loadDataFromDatabase();
-
-
     }
 
+    //Method assign to the back button in the create account page so when clicked
+    // it will go back to the homepage
+    public void goHomePageAction(ActionEvent event) throws IOException {
+        Parent hpParent = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        Scene hpScene = new Scene(hpParent);
+        Stage hpStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        hpStage.getIcons().add(new Image("/icons/TrendAssist Logo2.jpg"));
+        hpStage.setScene(hpScene);
+        hpStage.setTitle("Back");
+        hpStage.hide();
+        hpStage.show();
+    }
+
+    //Method assign to the log out button in the create account screen so when clicked
+    // it will return to the login screen.
+    public void goLoginAction(ActionEvent event) throws IOException {
+        Parent createParent = FXMLLoader.load(getClass().getResource("LogInScreen.fxml"));
+        Scene createScene = new Scene(createParent);
+        Stage createStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        createStage.getIcons().add(new Image("/icons/TrendAssist Logo2.jpg"));
+        createStage.setScene(createScene);
+        createStage.setTitle("Log Out");
+        createStage.hide();
+        createStage.show();
+    }
 
 
 }
--- trendAssistGUI/src/frontend/GenSalePredController.java
+++ trendAssistGUI/src/frontend/GenSalePredController.java
@@ -2,12 +2,15 @@ package frontend;
 import backend.testing;
 import backend.testing.*;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
@@ -26,46 +29,28 @@ import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class GenSalePredController implements Initializable {
-//    @FXML private TableView<UserDetails> table;
-//    @FXML private TableColumn<UserDetails, String> columnUser;
-//    @FXML private TableColumn<UserDetails, String> columnPass;
+
     @FXML private BarChart<String, Float> barChart;
     @FXML private DatePicker datePicker;
     @FXML private DatePicker datePicker1;
     @FXML private TextField Average;
+    @FXML private TableView<SalesDetails> table;
+    @FXML private TableColumn<SalesDetails, String> columnDate;
+    @FXML private TableColumn<SalesDetails, Float> columnAvgSales;
 
-//    private ObservableList<UserDetails> data;
+
     private DBconnection dc;
+    private ObservableList<SalesDetails> data;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         dc = new DBconnection();
     }
 
-//    public void loadDataFromDatabase() throws IOException {
-//        try {
-//            Connection con = dc.makeconnection();
-//            data = FXCollections.observableArrayList();
-//
-//            ResultSet rs = con.createStatement().executeQuery("SELECT * FROM users");
-//            while (rs.next()) {
-//                data.add(new UserDetails(rs.getString(2), rs.getString(4)));
-//            }
-//        }catch (SQLException ex){
-//            System.err.println("Error"+ex);
-//        }
-//
-//        columnUser.setCellValueFactory(new PropertyValueFactory<>("user"));
-//        columnPass.setCellValueFactory(new PropertyValueFactory<>("pass"));
-//
-//        table.setItems(null);
-//        table.setItems(data);
-//    }
-
 
     //Method that will populate a bar graph based on user selection of dates he/she
     // wants to check
-    @FXML
+
     public void populateBarGraph(ActionEvent event) throws IOException {
 
         //Retrieve values from datepickers field in the GUI, and from the date
@@ -75,14 +60,12 @@ public class GenSalePredController implements Initializable {
         Integer d1 = ld.getDayOfYear();
 
         LocalDate ld1 = datePicker1.getValue();
-        String dayOfWeek1 = ld1.getDayOfWeek().toString();
         Integer d2 = ld1.getDayOfYear();
 
         //A DateTimeFormatter instance to format the date to the way its stored
         // in the database
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        String date = ld.format(formatter);
-        String date1 = ld1.format(formatter);
+
 
         LocalDate temp[] = new LocalDate[(d2-d1)+1];
         String x[] = new String[(d2-d1)+1];
@@ -101,46 +84,37 @@ public class GenSalePredController implements Initializable {
         }
 
 
-
-        //Method call from the backend package to retrieve the average gross sales
-        // for a specific date, the convert the float value to string and set the
-        // the value of the Textfield in the GUI using the average
-        float avg = testing.frontGetAvg(date, dayOfWeek);
-
-
-        //Get the correct month name so we can use it to access the correct table
-        // in the database
-        String month = testing.WhatMonth(date);
-
-
-        String day = testing.getNumDay(date, dayOfWeek);
-        String day2 = testing.getNumDay(date1, dayOfWeek1);
-
         XYChart.Series<String, Float> series = new XYChart.Series<>();
         float sum = 0;
         try {
             Connection con = dc.makeconnection1();
+            data = FXCollections.observableArrayList();
             Integer p = 0;
             barChart.getData().clear();
             while(p <= (d2-d1)) {
                 PreparedStatement pstmt = con.prepareStatement("SELECT * FROM " + y[p] + " WHERE DayOfMonth = '" + z[p] + "'");
-                System.out.println(z[p] + " " + y[p] + " " + w[p]);
-                ResultSet rs = pstmt.executeQuery();
+
+                pstmt.executeQuery();
                 //Populate graph using database values
 
                     series.getData().add(new XYChart.Data<>(x[p], w[p]));
+                    data.add(new SalesDetails(x[p], w[p]));
                     sum += w[p];
-
                 p++;
             }
 
             String value = String.valueOf(sum/p);
             Average.setText(value);
             barChart.getData().add(series);
+
         }catch (SQLException ex){
             System.err.println("Error"+ex);
         }
-
+        columnDate.setCellValueFactory(new PropertyValueFactory<SalesDetails, String>("Date"));
+        columnAvgSales.setCellValueFactory(c ->
+                new ReadOnlyObjectWrapper<Float>( c.getValue().getAvgSales()) );
+        table.setItems(null);
+        table.setItems(data);
     }
 
 }
