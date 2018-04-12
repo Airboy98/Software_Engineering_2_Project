2a9f055b9857c9a7981ac638d2d70438e9d984da
AndreManz <32989529+AndreManz@users.noreply.github.com>
2018-04-12 17:31:11 -0500

    Backend Junit
    Updated failing tests

--- trendAssistGUI/src/backend/BackendTest.java
+++ trendAssistGUI/src/backend/BackendTest.java
@@ -12,15 +12,32 @@ public class BackendTest extends TestCase {
 		String str = String.valueOf(Answer); 
 		assertEquals(str,ans);
 		//Tests whatMonth, getNumDay, frontGetAvg and GetAvg at the same time
-	}
+	} 
+	public void testAvg1(){
+		String ans = "399.375";
+		float Answer = DbManager.frontGetAvg("8/26/2017", "sat");
+		String str = String.valueOf(Answer); 
+		assertEquals(str,ans);
+		//Fails test of whatMonth, getNumDay, frontGetAvg and GetAvg at the same time
+	} 
 	
 	public void testIntoDaily() {
 		float value = (float) (1300.45);
 		assertTrue(DbManager.IntoDaily("8/25/2017", "fri", value));
 	} //tests IntoDaily
 	
+	public void testIntoDaily1() {
+		float value = (float) (1300.45);
+		assertTrue(DbManager.IntoDaily("8/252019", "fri", value));
+	} //fails test of IntoDaily
+	//Should have failed but it didn't. It accepted the wrong input
+	
 	public void testDayOfMonth() {
 		assertEquals(DbManager.dayofmonth(20),3);
+	}//tests dayofmonth
+	
+	public void testDayOfMonth1() {
+		assertEquals(DbManager.dayofmonth(10),3);
 	}//tests dayofmonth
 	
 	public void testCSVupdate(){
@@ -30,6 +47,10 @@ public class BackendTest extends TestCase {
 	
 	public void testMonthUpdate() {
 		assertTrue(DbManager.MonthUpdate("12/29/2017"));
-	}//tests MonthUpdate
+	}//tests MonthUpdate 
+	
+	public void testMonthUpdate1() {
+		assertTrue(DbManager.MonthUpdate("1229/2017"));
+	}//tests MonthUpdate 
 	
 }
