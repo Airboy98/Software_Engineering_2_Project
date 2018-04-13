05b9548436c01bcef058a6541ea7d683722daeab
Aaron Riggs <arsgate@gmail.com>
2018-04-13 15:49:11 -0500

    consumer copy

---
+++ trendAssist/DataBase/salesdata_jun.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: salesdata
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jun`
+--
+
+DROP TABLE IF EXISTS `jun`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jun` (
+  `DayOfMonth` varchar(45) NOT NULL,
+  `AvgGrossSales` double NOT NULL,
+  PRIMARY KEY (`DayOfMonth`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jun`
+--
+
+LOCK TABLES `jun` WRITE;
+/*!40000 ALTER TABLE `jun` DISABLE KEYS */;
+INSERT INTO `jun` VALUES ('1fri',6341.0938),('1mon',796.77606),('1sat',5783.199),('1sun',747.1849),('1thu',2692.96),('1tue',1361.7471),('1wed',194.30385),('2fri',5266.1494),('2mon',856.4346),('2sat',6859.0674),('2sun',579.06934),('2thu',3575.1162),('2tue',1939.4443),('2wed',1250.8555),('3fri',6239.0957),('3mon',1307.6523),('3sat',5329.7773),('3sun',321.87735),('3thu',2630.4736),('3tue',1647.1836),('3wed',1283.1455),('4fri',6296.6357),('4mon',560.38184),('4sat',7116.407),('4sun',983.6172),('4thu',3146.459),('4tue',1495.0381),('4wed',1142.4268),('5fri',4428.992),('5mon',140.14923),('5sat',73.72943),('5sun',69.038605),('5thu',4809.292),('5tue',14.594082),('5wed',10.482414);
+/*!40000 ALTER TABLE `jun` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-12 16:36:19
